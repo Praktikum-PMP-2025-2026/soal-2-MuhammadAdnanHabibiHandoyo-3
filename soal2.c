d1bec298b625cfc0950a3db63757e2f92bdb2699
Adnhh <habibi7j@gmail.com>
2026-04-27 08:47:32 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -1,5 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void) {
+typedef struct Meteor{
+    char* nama;
+    int massa;
+    int kemurnian;
+}Meteor;
+
+int main(){
+    Meteor* arr = NULL;
+    int n;
+
+    scanf("%d", &n);
+    arr = malloc(n*sizeof(Meteor));
+    for(int i=0; i<n; i++){
+        arr[i].nama = (char*) malloc(100*sizeof(char));
+
+        scanf("%s %d %d", arr[i].nama,&arr[i].massa, &arr[i].kemurnian);
+    };
+
+    Meteor temp;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j+1].kemurnian > arr[j].kemurnian) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }else if(arr[j+1].kemurnian == arr[j].kemurnian && arr[j+1].massa < arr[j].massa){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }else if(arr[j+1].kemurnian == arr[j].kemurnian && arr[j+1].massa == arr[j].massa && strcmp(arr[j].nama, arr[j+1].nama) > 0){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    if(n<3){
+        for(int i=0; i<n;i++){
+            printf("%s %d %d\n", arr[i].nama, arr[i].massa, arr[i].kemurnian);
+        }
+    }else{
+        for(int i=0; i<3;i++){
+            printf("%s %d %d\n", arr[i].nama, arr[i].massa, arr[i].kemurnian);
+        }
+    }
     return 0;
 }
